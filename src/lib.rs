6e4203c2b9926693d509f746160ea3550a075f79
Trenton Andres <trenton.andres@gmail.com>
2020-08-19 21:32:49 -0500

    Draw a square, oh boy.

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,36 @@
 use log::trace;
+use wasm_bindgen::JsCast;
+use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::*;
-
+use web_sys::{Document, CanvasRenderingContext2d};
 const GIT_VERSION: &str = git_version::git_version!();
 
 #[wasm_bindgen(start)]
-pub fn main() -> Result<(), JsValue>{
+pub fn main() -> Result<(), JsValue> {
     console_log::init_with_level(log::Level::Trace).unwrap();
     console_error_panic_hook::set_once();
 
     trace!("Build: {}", GIT_VERSION);
     let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
+    let document: Document = window.document().expect("should have a document on window");
+    two_dimension_context(&document).expect("Failed to initialize 2d render context");
+    Ok(())
+}
 
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
-
-    body.append_child(&val)?;
-
+fn two_dimension_context(doc: &Document) -> Result<(), JsValue> {
+    let canvas = doc.get_element_by_id("rustCanvas").unwrap();
+    trace!("Element: {:?}", canvas);
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+    let context = canvas.get_context("2d")?;
+    trace!("Context: {:?}", context);
+    let render: CanvasRenderingContext2d = context
+        .ok_or(JsValue::from_str("wah wah"))?
+        .dyn_into()?;
+    let green = JsValue::from_str("green");
+    render.set_fill_style(&green);
+    render.fill_rect(10.0, 10.0, 100.0, 100.0);
     Ok(())
 }
 
